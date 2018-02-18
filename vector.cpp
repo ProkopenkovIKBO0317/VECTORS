ca0f9524816a154e079d09ab557da6326e332f97
Denis <32365426+ProkopenkovIKBO0317@users.noreply.github.com>
2018-02-18 03:13:26 +0300

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include "vector.h"
+#include "iostream"
+
+using namespace std;
+
+void Vector(double r1, double r2)
+{
+	cout << "Your vector is (" << r1 << ';' << r2 << ')' << endl;
+}
+
+double sum(double a, double b)
+{
+	return a + b;
+}
+
+double substraction(double a, double b)
+{
+	return a - b;
+}
+
+double multiply(double n, double coord)
+{
+	return n * coord;
+}
+
+
+
+
+
+
---
+++ vector.h
@@ -0,0 +1,12 @@
+#ifndef VECOR_H
+#define VECOTR_H
+#include <cmath>
+#include <string.h>
+
+void Vector(double r1, double r2);
+double sum(double a, double b);
+double substraction(double a, double b);
+double multiply(double n, double coord);
+
+#endif VECTOR_H
+
